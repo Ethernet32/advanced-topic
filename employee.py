60d6ae6b452c2428cd5ad39dad194f1d20272574
ethernet32 <ethan.ralphs@ucas-edu.net>
2024-11-04 11:49:57 -0700

    first commit

---
+++ employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, name, title):
+        self.name=name
+        self.title=title
+
+    def __str__(self):
+        return f"Name: {self.name}\nTitle: {self.title}"
+def greeting():
+    print('Hello from the "employee.py" module/page')
---
+++ main.py
@@ -0,0 +1,6 @@
+from employee import Employee, greeting
+
+emp = Employee("mar","VP of sales")
+
+print(emp)
+greeting()
