8b5a26cc4f8e515232bb34e42223b6987d0a6ae6
ethernet32 <ethan.ralphs@ucas-edu.net>
2024-11-06 11:19:07 -0700

    made some more notes

--- composition.py
+++ composition.py
@@ -9,20 +9,29 @@ class Car:
 
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
-
+    #fir other proggrammers and for debuing. Tel what class and all attributes
+    def __repr__(self):
+        return f"Car {self.make}, {self.model}, {self.year}, {self.engine}"
 class Engine:
     def __init__(self, configuration, displacement, horsepower, torque):
         self.configuration = configuration
         self.displacement = displacement
         self.horsepower = horsepower
         self.torque = torque
+    
     def ignite(self):
         print("vroom Vroom")
 
-
+    def __str__(self):
+        return f"The Engine is a {self.configuration} with {self.displacement} displacement, {self.horsepower} horsepower, and {self.torque} torpue"
+    def __repr__(self):
+        return f"Engine {self.configuration}, {self.displacement}, {self.horsepower}, {self.torque}"
 myEngine = Engine("V8", 5.8, 236, 344)
 myCar = Car("Mazda", "Mazda3", 2013, myEngine)
 
 #to access a composit class you must access where it is saved within the class
 print(myCar)
 myCar.engine.ignite()
+
+print(repr(myCar))
+print(repr(myEngine))
